f7252e646666410478267d9e778ea5dded42caae
Kevin Andres Aristizabal <karistizabal@navisaf.com>
2022-11-17 01:01:15 -0500

    add linq

---
+++ LinqSnippets/Snippets.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqSnippets
+{
+    public class Snippets
+    {
+        static public void BasicLinQ()
+        {
+            string[] cars =
+            {
+                "VW Golf",
+                "VW California",
+                "Audi A3",
+                "Audi A5",
+                "Fiat Punto",
+                "Seat Ibiza",
+                "Seat Leon"
+            };
+
+            var carList = from car in cars select car;
+            foreach (var car in carList)
+            {
+                Console.WriteLine(car);
+            }
+
+            var audiList = from car in cars where car.Contains("Audi") select car;
+            foreach (var car in audiList)
+            {
+                Console.WriteLine(car);
+            }
+
+        }
+
+        static public void LinqNumbers()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var processedNumberList =
+                numbers.Select(num => num * 3)
+                .Where(num => num != 9)
+                .OrderBy(num => num);
+
+        }
+
+        static public void SearchEamples()
+        {
+            List<string> textLis = new List<string>
+            {
+                "a",
+                "bx",
+                "as",
+                "f",
+                "h",
+                "fh",
+                "jk",
+                "vb",
+                "yt",
+            };
+
+            var first = textLis.First();
+
+            var cText = textLis.First(text => text.Equals("c"));
+
+            var jText = textLis.First(text => text.Contains("j"));
+
+            var firstzorder = textLis.FirstOrDefault(text => text.Contains("z"));
+
+            var lastzorder = textLis.LastOrDefault(text => text.Contains("z"));
+
+            var uniqueElements = textLis.Single();
+
+            var uniqueDefaultElements = textLis.SingleOrDefault();
+        }
+
+        
+    }
+}
---
+++ backendAppNet/Services/Services.cs
@@ -0,0 +1,59 @@
+﻿using backendAppNet.DataAccess;
+using backendAppNet.Models.DataModels;
+using backendAppNet.Models.Enums;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendAppNet.Services
+{
+    public class Services : ControllerBase
+    {
+        private readonly UniversityDBContext _context;
+
+        public Services(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<User>> GetUser(string email)
+        {
+            var user = await _context.Users.FindAsync(email);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudentsAdult()
+        {
+            return await _context.Students.Where(x => x.Dob <= DateTime.Now.AddYears(-18)).ToListAsync();
+        }
+
+
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudentsForCourse()
+        {
+            return await _context.Students.Where(x => x.Courses.Count() > 0 ).ToListAsync();
+        }
+
+
+        public async Task<ActionResult<IEnumerable<Courses>>> GetCoursesLevelForStudents(Levels level)
+        {
+            return await _context.Courses.Where(x => x.Level == level && x.Students.Count() > 0).ToListAsync();
+        }
+
+
+        public async Task<ActionResult<IEnumerable<Courses>>> GetCoursesLevelForCategory(Levels level, int IdCategory)
+        {                           ;
+            return await _context.Courses.Where(x => x.Level == level && x.Categories.Any(x => x.Id== IdCategory)).ToListAsync();
+        }
+
+        public async Task<ActionResult<IEnumerable<Courses>>> GetCoursesNoStudents()
+        {
+            return await _context.Courses.Where(x => x.Students.Count() == 0).ToListAsync();
+        }
+    }
+}
